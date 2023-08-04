8b57c60e4c349c3ff3db5ebb11caec3eaa9928b7
bmBMT <alexandr.samarin.04@mail.ru>
2023-08-04 17:27:28 +0300

    axios api url

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -5,7 +5,7 @@ export const API_URL = import.meta.env.VITE_API_URL
 
 const $api = axios.create({
   withCredentials: true,
-  baseURL: API_URL
+  baseURL: API_URL + '/api'
 })
 
 $api.interceptors.request.use((config) => {
